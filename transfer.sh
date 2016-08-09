d3e1db14f1a243c054cb61a3b8692051a8e6b4a7
GochoMugo <mugo@forfuture.co.ke>
2016-08-09 11:50:55 +0300

    Fix unquoted strings

--- transfer.sh
+++ transfer.sh
@@ -33,32 +33,31 @@ function transfer() {
   then
     basefile=$(basename "$file" | sed -e 's/[^a-zA-Z0-9._-]/-/g')
 
-    if [ ! -e $file ];
+    if [ ! -e "$file" ];
     then
-      echo "File $file doesn't exists."
+      echo "File '$file' doesn't exists."
       return 1
     fi
 
-    if [ -d $file ];
+    if [ -d "$file" ];
     then
       # zip directory and transfer
       zipfile=$( mktemp -t transferXXX.zip )
-      cd $(dirname $file) && zip -r -q - $(basename $file) >> $zipfile
-      curl --progress-bar --upload-file "$zipfile" "https://transfer.sh/$basefile.zip" >> $tmpfile
-      rm -f $zipfile
+      cd "$(dirname "$file")" && zip -r -q - "$(basename "$file")" >> "$zipfile"
+      curl --progress-bar --upload-file "$zipfile" "https://transfer.sh/$basefile.zip" >> "$tmpfile"
+      rm -f "$zipfile"
     else
       # transfer file
-      curl --progress-bar --upload-file "$file" "https://transfer.sh/$basefile" >> $tmpfile
+      curl --progress-bar --upload-file "$file" "https://transfer.sh/$basefile" >> "$tmpfile"
     fi
   else
     # transfer pipe
-    curl --progress-bar --upload-file "-" "https://transfer.sh/$file" >> $tmpfile
+    curl --progress-bar --upload-file "-" "https://transfer.sh/$file" >> "$tmpfile"
   fi
 
   # cat output link
-  cat $tmpfile
+  cat "$tmpfile"
 
   # cleanup
-  rm -f $tmpfile
+  rm -f "$tmpfile"
 }
-
